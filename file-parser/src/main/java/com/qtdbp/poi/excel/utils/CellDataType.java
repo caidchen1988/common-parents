71ce25034436e6720f60bfb8593c0f2dccec689d
caid <caidchen@126.com>
2017-07-19 18:50:49 +0800

    测试

---
+++ file-parser/src/main/java/com/qtdbp/poi/excel/utils/CellDataType.java
@@ -0,0 +1,10 @@
+package com.qtdbp.poi.excel.utils;
+
+/**
+ * @author: caidchen
+ * @create: 2017-07-19 17:49
+ * To change this template use File | Settings | File Templates.
+ */
+public enum CellDataType {
+    BOOL, ERROR, FORMULA, INLINESTR, SSTINDEX, NUMBER, DATE, NULL
+}
